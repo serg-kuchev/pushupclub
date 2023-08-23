8a96ae8abb13db2cfe657366199f87c1618e9db4
OlegStavchanskiy <olegswork@mail.ru>
2023-08-23 23:55:36 +0300

    bot

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 import psycopg2
 
-# comment
+#rap1dity
 
 #AIzaSyATqFOTz3ToTkDviXZRYu5L58-3mHMoQxI
 
