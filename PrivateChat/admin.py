3a7176b6b2111f3c1f7a675c1d20d61bdb2193ca
rap1dity <timeworstseal@gmail.com>
2023-08-27 14:20:47 +0300

    Open Beta 1.0.6. Stable build

--- PrivateChat/admin.py
+++ PrivateChat/admin.py
@@ -130,20 +130,3 @@ async def edit_section_concrete(callback: types.CallbackQuery, state: FSMContext
     await state.update_data(activity_type=activity[1], thread_id=int(activity[2]))
     await Activity.gid.set()
     await callback.message.edit_text('Добавьте gid номер листа(располагается в самом конце)')
-
-
-
-# mb future update
-# @dp.message_handler(state=Activity.begin_str)
-# async def activity_begin_str(message: types.Message, state: FSMContext):
-#     await state.update_data(begin_str=message.text)
-#     data = await state.get_data()
-#     await state.finish()
-#     try:
-#         cursor.execute(f"UPDATE activities SET gid='{data['gid']}', sp_id='{data['sp_id']}', str_id='{data['begin_str']}' WHERE thread_id={data['thread_id']}")
-#         connect.commit()
-#         await message.answer(f"Таблица {data['activity_type']} успешно создана")
-#     except Exception as e:
-#         print(e)
-#         connect.rollback()
-#         await message.answer(f'При создании таблицы произошла ошибка\n{e}\nОбратитесь с проблемой к разработчикам!')
